9bce3e2dbdd9730a68db14405ec6f71e75d8a867
Daniil-Starushenko <daniil80801@gmail.com>
2021-05-26 13:15:54 +0300

    additional libraries added

--- task1/src/main/java/com/leverX/lab/Main.java
+++ task1/src/main/java/com/leverX/lab/Main.java
@@ -1,7 +1,9 @@
 package com.leverx.lab;
+import org.apache.commons.math.complex.Complex;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world");
+        Complex complex = new Complex(1, 2);
     }
 }
