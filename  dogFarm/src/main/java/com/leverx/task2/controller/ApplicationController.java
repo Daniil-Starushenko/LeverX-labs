e347e5200cf534eeb5c2a15d0ac036be5bfb34f9
Daniil-Starushenko <daniil80801@gmail.com>
2021-06-09 13:04:09 +0300

    controller

---  dogFarm/src/main/java/com/leverx/task2/controller/ApplicationController.java
+++  dogFarm/src/main/java/com/leverx/task2/controller/ApplicationController.java
@@ -1,6 +1,7 @@
 package com.leverx.task2.controller;
 
-import com.leverx.task2.entity.Dog;
+import com.leverx.task2.entity.*;
+import com.leverx.task2.service.Day;
 import com.leverx.task2.service.DogCreator;
 
 import org.apache.log4j.Logger;
@@ -17,9 +18,15 @@ class ApplicationController {
     private static final Logger logger = Logger.getLogger(ApplicationController.class.getName());
 
     private DogCreator dogCreator;
+    private Vet vet;
+    private Caretakers caretakers;
+    private Trainer trainer;
 
     public ApplicationController() {
         dogCreator = new DogCreator();
+        vet = new Vet();
+        caretakers = new Caretakers();
+        trainer = new Trainer();
     }
 
     /**
@@ -27,11 +34,29 @@ class ApplicationController {
      * and runs the main service method <code></code>
      */
     protected void run() {
+        List<Dog> dogs = new ArrayList<>();
+        dogs = initializeDogs();
+
+        List<Aviary> aviaries = new ArrayList<>();
+        for (Dog dog : dogs) {
+            aviaries.add(new Aviary(dog));
+        }
+
+        Day day = new Day(aviaries, dogs, vet, caretakers, trainer);
+        day.emulateDay();
+    }
+
+    /**
+     * method that initialize list of <code>Dog</code> objects
+     * @return initialized list of <code>Dog</code> objects
+     */
+    private List<Dog> initializeDogs() {
         Random random = new Random();
-        List<Dog> dogs = new ArrayList<Dog>();
+        List<Dog> dogs = new ArrayList<>();
         for (int i = 1; i <= (random.nextInt(20) + 6); i++) {
             dogs.add(dogCreator.initializeDog(i));
         }
-        logger.info("dogs are  created");
+        logger.info("dogs are created");
+        return dogs;
     }
 }
---  dogFarm/src/main/java/com/leverx/task2/service/Day.java
+++  dogFarm/src/main/java/com/leverx/task2/service/Day.java
@@ -1,6 +1,25 @@
 package com.leverx.task2.service;
 
+import com.leverx.task2.entity.*;
+
+import java.util.List;
+
 public class Day {
+    private List<Aviary> aviaries;
+    private List<Dog> dogs;
+    private Vet vet;
+    private Caretakers caretakers;
+    private Trainer trainer;
 
+    public Day(List<Aviary> aviaries, List<Dog> dogs, Vet vet, Caretakers caretakers, Trainer trainer) {
+        this.aviaries = aviaries;
+        this.dogs = dogs;
+        this.vet = vet;
+        this.caretakers = caretakers;
+        this.trainer = trainer;
+    }
 
+    public void emulateDay() {
+
+    }
 }
