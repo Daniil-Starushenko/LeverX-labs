4937c95a72acd6880b62afff28273a89fe38adc8
Daniil-Starushenko <daniil80801@gmail.com>
2021-06-10 12:32:28 +0300

    complete

---  dogFarm/src/main/java/com/leverx/task2/dal/OutputFileWriter.java
+++  dogFarm/src/main/java/com/leverx/task2/dal/OutputFileWriter.java
@@ -1,11 +1,15 @@
 package com.leverx.task2.dal;
 
+import org.apache.log4j.Logger;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 public class OutputFileWriter {
+    private static final Logger logger = Logger.getLogger(OutputFileWriter.class.getName());
+
     private String path;
     private final File file;
 
@@ -23,7 +27,7 @@ public class OutputFileWriter {
                 fileWriter.write(line + System.lineSeparator());
             }
         } catch (IOException e) {
-            System.out.println("problems with writing in file: " + e);
+            logger.error("problems with writing in file" + e);
         }
     }
 }
